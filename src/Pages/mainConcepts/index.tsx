17733475b2ee4dea18d9d0f4c7491e93dbf8f743
Marcos Botene <marcosjbotene@gmail.com>
2020-09-15 16:13:53 -0300

    Adicionando Métodos de Ciclo de Vida a Classe.

--- src/Pages/mainConcepts/index.tsx
+++ src/Pages/mainConcepts/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { isPropertySignature } from 'typescript';
+import { isDate } from 'util';
 
 import './style.css';
 
@@ -97,12 +98,41 @@ function UserInfo(props: AvatarProps) {
 //Estado e Ciclo de Vida
 //===============================================
 
-class Clock extends React.Component {
+interface ClockProps {
+  date: Date;
+  timerID: number;
+  tick: () => Date;
+}
+
+class Clock extends React.Component<{}, ClockProps> {
+  constructor(props: ClockProps) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      timerID: 0,
+      tick: () => new Date(),
+    };
+  }
+
+  componentDidMount(this: ClockProps) {
+    this.timerID = window.setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount(this: ClockProps) {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Hello, world!</h3>
-        <h3>It is {}.</h3>
+        <h3>It is {this.state.date.toLocaleTimeString()}.</h3>
       </div>
     );
   }
@@ -207,21 +237,39 @@ const mainConcepts = () => {
         cinco etapas:
       </p>
       <ul>
-        <ol>
+        <li>
           Criar uma classe ES6, com o mesmo nome, estendendo React.component.
-        </ol>
-        <ol>Adicionar um único método vazio chamado render().</ol>
-        <ol>Mova o corpo da função para o método render().</ol>
-        <ol>Substitua props por this.props no corpo de render().</ol>
-        <ol>Exclua a declaração da função vazia restante.</ol>
+        </li>
+        <li>Adicionar um único método vazio chamado render().</li>
+        <li>Mova o corpo da função para o método render().</li>
+        <li>Substitua props por this.props no corpo de render().</li>
+        <li>Exclua a declaração da função vazia restante.</li>
       </ul>
+      <h2>Adicionando Métodos de Ciclo de Vida a Classe</h2>
       <p>
-        O método render será chamado toda vez que uma atualização acontecer, mas
-        enquanto renderizarmos {'<Clock>'} no mesmo nó DOM, apenas uma única
-        instância da classe Clock será usada. Isso nos permite usar recursos
-        adicionais, como o estado local e os métodos de ciclo de vida.
+        Em aplicações com muitos componentes, é muito importante limpar os
+        recursos utilizados pelos componentes quando eles são destruídos.
       </p>
-      <h2>Adicionando Estado Local a uma Classe</h2>
+      <p>
+        Quando a saída do Clock é inserida no DOM, o React chama o método do
+        ciclo de vida componentDidMount(). Dentro dele, o componente Clock pede
+        ao navegador para configurar um temporizador para chamar o método tick()
+        do componente uma vez por segundo.
+      </p>
+      <p>
+        A cada segundo o navegador chama o método tick(). Dentro dele, o
+        componente Clock agenda uma atualização de UI chamando setState() com um
+        objeto contendo a hora atual. Graças à chamada setState(), o método
+        render() será diferente e, portanto, a saída de renderização incluirá a
+        hora atualizada.
+      </p>
+      <p>
+        Se o componente Clock for removido do DOM, o React chama o método do
+        ciclo de vida componentWillUnmount() para que o temporizador seja
+        interrompido.
+      </p>
+      <p>Resultado:</p>
+      <Clock />
     </div>
   );
 };
