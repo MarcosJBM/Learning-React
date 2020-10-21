9119f398c0c3855e04a8f2e1a06d1d2b8f1a1b81
Marcos Botene <marcosjbotene@gmail.com>
2020-10-21 19:37:35 -0300

    Fix.

--- src/Pages/mainConcepts/RaisingTheState/index.jsx
+++ src/Pages/mainConcepts/RaisingTheState/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit',
+  c: "Celsius",
+  f: "Fahrenheit",
 };
 
 function toCelsius(fahrenheit) {
@@ -16,7 +16,7 @@ function toFahrenheit(celsius) {
 function tryConvert(temperature, convert) {
   const input = parseFloat(temperature);
   if (Number.isNaN(input)) {
-    return '';
+    return "";
   }
   const output = convert(input);
   const rounded = Math.round(output * 1000) / 1000;
@@ -37,7 +37,7 @@ class TemperatureInput extends React.Component {
   }
 
   handleChange(event) {
-    this.props.onTemperatureChange(event.target.value)
+    this.props.onTemperatureChange(event.target.value);
   }
 
   render() {
@@ -58,35 +58,38 @@ class Calculator extends React.Component {
     super(props);
     this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
     this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-    this.state = { temperature: '', scale: 'c' };
+    this.state = { temperature: "", scale: "c" };
   }
 
   handleCelsiusChange(temperature) {
-    this.setState({ scale: 'c', temperature });
+    this.setState({ scale: "c", temperature });
   }
 
   handleFahrenheitChange(temperature) {
-    this.setState({ scale: 'f', temperature });
+    this.setState({ scale: "f", temperature });
   }
 
   render() {
     const scale = this.state.scale;
     const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+    const celsius =
+      scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
 
     return (
       <div>
         <TemperatureInput
           scale="c"
           temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange} />
+          onTemperatureChange={this.handleCelsiusChange}
+        />
         <TemperatureInput
           scale="f"
           temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange} />
-        <BoilingVerdict
-          celsius={parseFloat(celsius)} />
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
       </div>
     );
   }
@@ -112,8 +115,8 @@ export default function RaisingTheState() {
       </p>
       <BoilingVerdict celsius={100} />
       <p>
-        A seguir, criaremos um componente chamado Calculator. Ele renderiza um{' '}
-        {'<input>'} que recebe a temperatura e a mantém em
+        A seguir, criaremos um componente chamado Calculator. Ele renderiza um{" "}
+        {"<input>"} que recebe a temperatura e a mantém em
         this.state.temperature.
       </p>
       <p>
@@ -192,7 +195,7 @@ export default function RaisingTheState() {
       </p>
       <p>
         No React, isso é comumente solucionado ao tornar um componente comum em
-        um “componente controlado”. Assim como o {'<input>'} do DOM aceita ambas
+        um “componente controlado”. Assim como o {"<input>"} do DOM aceita ambas
         as props value e onChange, o componente personalizado TemperatureInput
         também pode aceitar ambas as props temperature e onTemperatureChange do
         Calculator, seu componente pai.
