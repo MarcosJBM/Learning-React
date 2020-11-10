9148ff897cb9cd3eec9e4b89a5b17c035576c71d
Marcos Botene <marcosjbotene@gmail.com>
2020-11-10 00:48:30 -0200

    I will fix this later.

--- src/Pages/AdvancedGuides/index.tsx
+++ src/Pages/AdvancedGuides/index.tsx
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 export default function AdvancedGuides() {
   return (
     <div>
-      <Link to='/CodeSplitting'>Dividindo o Código</Link>
+      <Link to="/CodeSplitting">Dividindo o Código</Link>
       <br />
-      <Link to='/Context'>Contexto</Link>
+      <Link to="/Context">Contexto</Link>
       <br />
-      <Link to='/ErrorBoundaries'>Error Boundaries</Link>
+      <Link to="/ErrorBoundaries">Error Boundaries</Link>
       <br />
-      <Link to='/Fragments'>Fragmentos</Link>
+      <Link to="/Fragments">Fragmentos</Link>
       <br />
-      <Link to='/JSXInDepth'>JSX In Depth</Link>
+      <Link to="/JSXInDepth">JSX In Depth</Link>
       <br />
-      <Link to='/OptimizingPerformance'>Otimizando o Desempenho</Link>
+      <Link to="/OptimizingPerformance">Otimizando o Desempenho</Link>
+      <br />
+      <Link to="/Portals">Portals</Link>
     </div>
   );
 }
